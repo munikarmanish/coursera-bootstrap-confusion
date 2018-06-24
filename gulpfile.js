7e324fff878c742e43ace36cb8897f171a321233
Manish Munikar <munikarmanish@gmail.com>
2018-06-24 15:27:22 +0545

    exercise: gulp (part 2)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var cleanCss = require('gulp-clean-css');
+var flatmap = require('gulp-flatmap');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
@@ -30,4 +38,45 @@ gulp.task('browser-sync', function () {
 
 gulp.task('default', ['browser-sync'], function () {
     gulp.start('sass:watch');
+});
+
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function () {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true,
+        }))
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function (stream, file) {
+            return stream.pipe(usemin({
+                css: [rev()],
+                html: [function () {
+                    return htmlmin({
+                        collapseWhitespace: true
+                    })
+                }],
+                js: [uglify(), rev()],
+                inlinejs: [uglify()],
+                inlinecss: [cleanCss()],
+            }));
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.start('copyfonts', 'imagemin', 'usemin');
 });
