b76a50b0012a8478f45201b5e6b0bc17615b6eb9
Manish Munikar <munikarmanish@gmail.com>
2018-06-24 14:48:15 +0545

    exercise: gulp (part 1)

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {
+    return gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function () {
+    var files = [
+        './*.html',
+        './css/*.css',
+        './js/*.js',
+        './img/*.{png,jpg,gif}'
+    ];
+    browserSync.init(files, {
+        server: {
+            baseDir: './',
+        },
+    })
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.start('sass:watch');
+});
